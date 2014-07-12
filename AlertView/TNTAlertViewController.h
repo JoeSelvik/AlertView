876026c7c37495284f032016b28c61f1b4fcb415
JoeSelvik <joe@thenewtricks.com>
2014-07-11 20:58:11 -0600

    added a SB AlertView and a corresponding class

---
+++ AlertView/TNTAlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  TNTAlertViewController.h
+//  AlertView
+//
+//  Created by Joe Selvik on 7/11/14.
+//  Copyright (c) 2014 The New Tricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNTAlertViewController : UIViewController
+
+@end
