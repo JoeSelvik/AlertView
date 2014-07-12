c569b23fe4c2b0b885877a77daeb4aaea9cdc310
JoeSelvik <joe@thenewtricks.com>
2014-07-11 18:10:14 -0600

    init

---
+++ AlertView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlertView
+//
+//  Created by Joe Selvik on 7/11/14.
+//  Copyright (c) 2014 The New Tricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TNTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TNTAppDelegate class]));
+    }
+}
---
+++ AlertViewTests/AlertViewTests.m
@@ -0,0 +1,34 @@
+//
+//  AlertViewTests.m
+//  AlertViewTests
+//
+//  Created by Joe Selvik on 7/11/14.
+//  Copyright (c) 2014 The New Tricks. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlertViewTests : XCTestCase
+
+@end
+
+@implementation AlertViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
