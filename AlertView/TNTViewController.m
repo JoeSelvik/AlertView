653fb0aa00b472484e519367a4e6b584e2a48a76
JoeSelvik <joe@thenewtricks.com>
2014-07-15 22:42:10 -0600

    can now add more alertmessageVC’s

--- AlertView/TNTViewController.m
+++ AlertView/TNTViewController.m
@@ -11,8 +11,9 @@
 @interface TNTViewController ()
 
 @property (assign, nonatomic) CGRect frameForAlertView;
-
 @property (nonatomic, weak) UIViewController *currentAlertMessageViewController;
+
+- (IBAction)newAlertVCButton:(id)sender;
 
 @end
 
@@ -60,38 +61,8 @@
     TNTAlertViewController *alertVC = [storyboard instantiateViewControllerWithIdentifier:@"alertViewController"];
     alertVC.view.frame = self.frameForAlertView;
     
-//    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
-//    [alertVC.view addGestureRecognizer:tap];
-    
     return alertVC;
 }
-
-//- (void)tap:(UIGestureRecognizer *)gr
-//{
-//    if (gr.state == UIGestureRecognizerStateEnded)
-//        [self transitionToNextViewController];
-//}
-
-//- (void)transitionToNextViewController
-//{
-////    TNTAlertViewController *alertVC = [self alertVC];
-////    [alertVC setAlertMessage:@"You are number 666"];
-//
-////    // Containment
-////    [self addChildViewController:alertVC];
-////    [self.currentAlertMessageViewController willMoveToParentViewController:nil];
-////
-////    [alertVC didMoveToParentViewController:self];
-////    [self.currentAlertMessageViewController removeFromParentViewController];
-////    self.currentAlertMessageViewController = alertVC;
-//
-//    // Remove alertVC
-//    [self.currentAlertMessageViewController willMoveToParentViewController:nil];
-//    [self.currentAlertMessageViewController.view removeFromSuperview];
-//    [self.currentAlertMessageViewController removeFromParentViewController];
-//    
-//}
-
 
 - (void)viewDidLayoutSubviews
 {
@@ -100,6 +71,22 @@
     self.currentAlertMessageViewController.view.layer.shadowPath = [UIBezierPath bezierPathWithRoundedRect:self.currentAlertMessageViewController.view.bounds cornerRadius:8].CGPath;
 }
 
+
+-(void)newAlertVCButton:(id)sender
+{
+    // Add an initial contained viewController
+    TNTAlertViewController *alertVC = [self alertVC];
+    [alertVC setAlertMessage:@"From button"];
+    [alertVC connectAlertVCCloseButtonWithSelf:alertVC];
+    
+    // Contain the view controller
+    [self addChildViewController:alertVC];
+    [self.view addSubview:alertVC.view];
+    [alertVC didMoveToParentViewController:self];
+    
+    // Keep track of the alertVC
+    self.currentAlertMessageViewController = alertVC;
+}
 
 
 @end
